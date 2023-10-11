f3926105d68f1a62b4e964ecfc45d81af8b16d33
Le Cong Danh <userforcodecommit-at-825656334157>
2023-10-11 23:19:58 +0700

    check unique email

--- src/main/java/danh/com/Clone/Ecomerce/repository/UserRepository.java
+++ src/main/java/danh/com/Clone/Ecomerce/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package danh.com.Clone.Ecomerce.repository;
 
 import danh.com.Clone.Ecomerce.entity.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    public User getUserByEmail(@Param("email") String email);
+
 }
--- src/main/java/danh/com/Clone/Ecomerce/service/UserService.java
+++ src/main/java/danh/com/Clone/Ecomerce/service/UserService.java
@@ -19,4 +19,10 @@ public class UserService {
     public void saveUser(User user) {
         userRepository.save(user);
     }
+
+    public boolean isEmailUnique(String email) {
+        User userByEmail = userRepository.getUserByEmail(email);
+
+        return userByEmail == null;
+    }
 }
